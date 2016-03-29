72c0c8c882fcb7d09b1f59b727808e29bb2d2c61
necsashi <necsashi@indiana.edu>
2016-03-29 15:11:04 -0400

    tquit quit

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/*Get Index page */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'index' });
+});
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'index' });
+});
+/* GET About page */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Us' });
+});
+
+/*Get Testimonials page */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Us' });
+});
+
+/*Get Contact page */
+router.get('/testimonials', function(req, res, next) {
+  res.render('testimonials', { title: 'Testimonials' });
+});
+
+
+module.exports = router;
